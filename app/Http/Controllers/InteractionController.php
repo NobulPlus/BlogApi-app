df50be30ce9f0caceea13621c6f7e1bfe4ea3c0f
NobulPlus <chenko4me@gmail.com>
2024-07-24 03:00:39 +0100

    All Controller function created

--- app/Http/Controllers/InteractionController.php
+++ app/Http/Controllers/InteractionController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class InteractionController extends Controller
 {
-    //
+    public function like(Post $post, Request $request)
+    {
+        $like = Like::create([
+            'post_id' => $post->id,
+            'user_id' => $request->user()->id,
+        ]);
+
+        return $like;
+    }
+
+    public function comment(Post $post, Request $request)
+    {
+        $request->validate([
+            'content' => 'required|string',
+        ]);
+
+        $comment = Comment::create([
+            'post_id' => $post->id,
+            'user_id' => $request->user()->id,
+            'content' => $request->content,
+        ]);
+
+        return $comment;
+    }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
