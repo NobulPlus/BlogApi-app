6a65c3243a21f7d68e615cbb969220d2c28532c6
NobulPlus <chenko4me@gmail.com>
2024-07-24 02:27:55 +0100

    second commit

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\InteractionController;
+
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Blog Endpoints
+Route::apiResource('blogs', BlogController::class);
+
+// Post Endpoints under a Specific Blog
+Route::apiResource('blogs.posts', PostController::class);
+
+Route::get('/', function () {
+    return 'API';
+});
