389b4d90b6dccbf5bb75b23da92f96e85171cb01
NobulPlus <chenko4me@gmail.com>
2024-07-24 03:00:21 +0100

    All Controller function created

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,45 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index(Blog $blog)
+    {
+        return $blog->posts;
+    }
+
+    public function store(Request $request, Blog $blog)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $post = new Post($request->all());
+        $blog->posts()->save($post);
+
+        return $post;
+    }
+
+    public function show(Blog $blog, Post $post)
+    {
+        return $post->load(['likes', 'comments']);
+    }
+
+    public function update(Request $request, Blog $blog, Post $post)
+    {
+        $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'content' => 'sometimes|string',
+        ]);
+
+        $post->update($request->all());
+
+        return $post;
+    }
+
+    public function destroy(Blog $blog, Post $post)
+    {
+        $post->delete();
+
+        return response()->noContent();
+    }
 }
